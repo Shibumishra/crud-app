6cacc582fe9a7a4af4247bf0f7845a793569f8fd
satyabrat.mishra <satyabrat.mishra@nirmitee.io>
2022-08-12 11:19:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AllUsers from './Components/AllUsers';
+import AddUser from './Components/AddUser';
+import EditUser from './Components/EditUser';
+import NavBar from './Components/NavBar';
+import NotFound from './Components/NotFound'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<AllUsers />} />
+        <Route path="/add" element={<AddUser />} />
+        <Route path="/edit/:id" element={<EditUser />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
